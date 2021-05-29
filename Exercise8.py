a4aeaae4eedc565242d00f1fe30cbbe3c698d583
= <=>
2021-05-29 20:30:37 +0300

    Exercise 8

--- Exercise8.py
+++ Exercise8.py
@@ -24,4 +24,14 @@ for item in words:
         longestWords.append(item)
 
 print(longestWords)
+
+###################################################################################
+
+def longest_word(filename):
+    with open(filename, 'r') as infile:
+              words = infile.read().split()
+    max_len = len(max(words, key=len))
+    return [word for word in words if len(word) == max_len]
+
+print(longest_word('test.txt'))
     
